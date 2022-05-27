18e7fbd7e1f404c7943921d317dfb03a55f20aed
Jonathan Cushing <firex42069@gmail.com>
2022-05-27 17:24:51 -0400

    why does vs code automatically add that sometimes

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # E-commerce Back End Starter Code
 
-route  | category | product  | tag
--------|----------|----------|---------------
-get    | woke     | broke    | functional
-getid  | woke     | broke    | functional
-post   | woke     | *        | woke
-put    | woke     | *        | woke
-delete | woke     | untested | woke
+route  | category | product    | tag
+-------|----------|------------|---------------
+get    | woke     | functional | functional
+getid  | woke     | functional | functional
+post   | woke     | *          | woke
+put    | woke     | *          | woke
+delete | woke     | untested   | woke
 
 *starter code provided these
--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-const { post } = require('../routes/api/product-routes');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
@@ -33,17 +32,17 @@ ProductTag.belongsTo(Product, {
   foreignKey: "product_id"
 });
 
-/*ProductTag.belongsTo(Tag, {
+ProductTag.belongsTo(Tag, {
   foreignKey: "tag_id"
-});*/
+});
 
 Product.hasMany(ProductTag, {
   foreignKey: "product_id"
 });
 
-/*Tag.hasMany(ProductTag, {
+Tag.hasMany(ProductTag, {
   foreignKey: "tag_id"
-});*/
+});
 
 module.exports = {
   Product,
